11cd229d335c09a3922a092d39815813589ec876
Rodas Getachew <getachewrodas39@gmail.com>
2024-05-20 00:08:50 +0300

    Airbnb_clone

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
-# models/__init__.py
+#!/usr/bin/python3
+"""__init__ magic method for models directory"""
+from models.engine.file_storage import FileStorage
 
-from.engine.file_storage import FileStorage
 
-# Create a unique FileStorage instance for your application
 storage = FileStorage()
-
-# Call the reload method on the storage instance
 storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,62 +1,49 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class."""
 import json
-import os
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+
 
 class FileStorage:
-    """
-    Class that serializes instances to a JSON file and deserializes JSON file to instances.
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
     """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """
-        Returns the dictionary __objects.
-        """
-        return self.__objects
+        """Return the dictionary __objects."""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """
-        Sets in __objects the obj with key <obj class name>.id.
-        """
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
-    
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+
     def save(self):
-        """
-        serializes __objects to the JSON file (path: __file_path)
-        """
-        json_objects = {}
-              # Iterate over all items in __objects
-        for key, obj in self.__objects.items():
-            # Convert each object to a dictionary using its to_dict method
-            obj_dict = obj.to_dict()
-            
-            # Add the dictionary representation of the object to json_objects
-            json_objects[key] = obj_dict
+        """Serialize __objects to the JSON file __file_path."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
 
-            with open(self.__file_path, 'w') as f:
-            #serialize the json_objects dictonry to the json file 
-                json.dump(json_objects, f)
-    
     def reload(self):
-        """
-        deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ;
-        otherwise, do nothing. 
-        If the file doesn’t exist, no exception should be raised)
-
-        """
-        def reload(self):
-            """
-            Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists;
-            otherwise, do nothing. If the file doesn’t exist
-            , no exception should be raised).
-            """
-            try:
-                with open(self.__file_path, 'r') as f:
-                    loaded_objects = json.load(f)
-                    for key, value in loaded_objects.items():
-                        self.__objects[key] = value
-            except FileNotFoundError:
-                pass  # No action needed if the file doesn't exist
-
-
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
